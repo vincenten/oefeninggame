216f63010f3b930329aa7718db66e93ed84f8e5a
Vincent Risack <risackvincent@hotmail.com>
2014-11-04 11:13:17 +0100

    added table list

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ActiveRecord::Base
 	belongs_to :genre
 	validates :name, length: {maximum: 32}, :presence => true
 	validates :genre_id, :presence => true
-	validates :description, length: {maximum: 32}
 	validates :release_year, length: {maximum: 32}, :presence => true
     validates :release_year, :inclusion => { :in => 1900..2100, :message => "The year must be between 1900 and 2100" }, length: {maximum: 32}, :presence => true
+mount_uploader :image, ImageUploader
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,5 +2,5 @@ class Genre < ActiveRecord::Base
 	has_many :games
 	validates :name, length: { maximum: 32}, :presence => true
 	validates :description, length: { maximum: 200}, :presence => true
-
 end
+
